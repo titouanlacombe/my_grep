816cc9d33589ce57e1fb42fd4885d747d2df066b
Titouan Lacombe <titouan.lacombe99@gmail.com>
2022-02-01 09:41:19 +0100

    start

--- my_grep.cpp
+++ my_grep.cpp
@@ -9,20 +9,25 @@
 using namespace std;
 using namespace std::chrono;
 
-// Help to print special characters
-string int_to_string(int result)
+// Print special characters correctly
+void int_to_str(int result, string &output)
 {
 	switch (result) {
 	case '\n':
-		return string("\\n");
+		output.append("\\n");
+		return;
 	case '\r':
-		return string("\\r");
+		output.append("\\r");
+		return;
 	case '\t':
-		return string("\\t");
+		output.append("\\t");
+		return;
 	case EOF:
-		return string("EOF");
+		output.append("EOF");
+		return;
 	default:
-		return string(1, result);
+		output.push_back(result);
+		return;
 	}
 }
 
@@ -50,24 +55,13 @@ public:
 // Store data about a match in the text
 class Match
 {
-	int start_line_nb; // Line number of the start position of the match
-	string context_start; // Text before the match and on the same line
-	string context_end; // Text after the match and on the same line
-	string extracted; // Extracted data by the regex
+	streampos start, end;
 
 public:
-	Match(int line_nb, string &_context_start)
+	Match(streampos &_start, streampos &_end)
 	{
-		start_line_nb = line_nb;
-		context_start = _context_start;
-	}
-
-	// Executed when a match succeed
-	// complete the data of the match
-	void complete(stringstream& _extracted, string &_context_end)
-	{
-		extracted = _extracted.str();
-		context_end = _context_end;
+		start = _start;
+		end = _end;
 	}
 
 	// Prints pretty line number with padding & a separator
@@ -87,31 +81,28 @@ public:
 	// Diplay the match with line number & it's context
 	void print(ostream& output)
 	{
-		stringstream stream(extracted);
-
 		// Match context start
-		print_line_nb(output, start_line_nb);
-		output << context_start << "\e[44m";
+		// print_line_nb(output, start_line_nb);
+		// output << context_start << "\e[44m";
 
-		// Match extracted data
-		int line_nb = start_line_nb;
-		while (stream.peek() != EOF) {
-			char c = stream.get();
+		// int line_nb = start_line_nb;
+		// while (stream.peek() != EOF) {
+		// 	char c = stream.get();
 
-			if (c == '\n') {
-				line_nb++;
-				// Remove color for the new line
-				output << "\\n\e[0m" << endl;
-				print_line_nb(output, line_nb);
-				output << "\e[44m";
-			}
-			else {
-				output << c;
-			}
-		}
+		// 	if (c == '\n') {
+		// 		line_nb++;
+		// 		// Remove color for the new line
+		// 		output << "\\n\e[0m" << endl;
+		// 		print_line_nb(output, line_nb);
+		// 		output << "\e[44m";
+		// 	}
+		// 	else {
+		// 		output << c;
+		// 	}
+		// }
 
 		// Match context end
-		output << "\e[0m" << context_end << endl;
+		// output << "\e[0m" << context_end << endl;
 	}
 };
 
@@ -119,12 +110,6 @@ public:
 class RegexOperator
 {
 public:
-	// Execute the regex operator on the input
-	// The matched data (if any) will be written to output
-	// The next operator will be used by lazy operators to know when to stop
-	// Return if the operation was a success
-	virtual bool execute(istream& input, ostream& output, RegexOperator* next) = 0;
-
 	// Convert the operator to a readable string for debbuging purposes
 	virtual string toString() = 0;
 };
@@ -141,24 +126,9 @@ public:
 		character = c;
 	}
 
-	virtual bool execute(istream& input, ostream& output, RegexOperator* next)
-	{
-		int i = input.get();
-
-		// If the char is found
-		if (i == character) {
-			// add it to matched data
-			output << (char)i;
-			// return success
-			return true;
-		}
-
-		return false;
-	}
-
 	virtual string toString()
 	{
-		return "Basic(" + int_to_string(character) + ")";
+		return "Basic(" + int_to_str(character) + ")";
 	}
 };
 
@@ -166,19 +136,6 @@ public:
 class RegexAnyChar : public RegexOperator
 {
 public:
-	virtual bool execute(istream& input, ostream& output, RegexOperator* next)
-	{
-		int c = input.get();
-
-		// can only fail if EOF is reached
-		if (c == EOF) {
-			return false;
-		}
-
-		output << (char)c;
-		return true;
-	}
-
 	virtual string toString()
 	{
 		return "Any()";
@@ -189,39 +146,6 @@ public:
 class RegexKleenStar : public RegexOperator
 {
 public:
-	virtual bool execute(istream& input, ostream& output, RegexOperator* next)
-	{
-		// if no next operator
-		if (next == nullptr) {
-			// match until end of file & return true
-			while (input.peek() != EOF) {
-				output << (char)input.get();
-			}
-			return true;
-		}
-
-		// if next operator exist
-		// match until it succeed
-		streampos pos = input.tellg(); // Save pos
-		bool match_success;
-		do {
-			stringstream _output;
-			match_success = next->execute(input, _output, nullptr);
-
-			if (!match_success) {
-				input.seekg(pos); // Indent & restore pos
-				output << (char)input.get();
-				pos = input.tellg();
-			}
-			else {
-				input.seekg(pos); // Indent & restore pos
-			}
-		}
-		while (!match_success && input.peek() != EOF);
-
-		return match_success;
-	}
-
 	virtual string toString()
 	{
 		return "Star()";
@@ -239,40 +163,6 @@ public:
 		for (RegexOperator* option : options) {
 			delete option;
 		}
-	}
-
-	virtual bool execute(istream& input, ostream& output, RegexOperator* next)
-	{
-		streampos pos = input.tellg(); // Save pos
-
-		// Foreach options
-		for (RegexOperator* option : options) {
-			// Create a temporary stream to store matched data
-			stringstream _output;
-
-			input.seekg(pos); // Restore pos of input
-
-			// Test option
-			bool match_success = option->execute(input, _output, next);
-
-			// If next exist
-			if (next != nullptr) {
-				// Test if the next operator is a success
-				match_success &= next->execute(input, _output, nullptr);
-			}
-
-			// If option is succesfull take it's matched extracted & return
-			if (match_success) {
-				// Ajoute a l'output la data matched
-				output << _output.str();
-				return true;
-			}
-		}
-
-		// If no option where succesfull
-		input.seekg(pos); // Restore pos
-
-		return false;
 	}
 
 	virtual string toString()
@@ -296,37 +186,6 @@ public:
 		for (RegexOperator* op : *this) {
 			delete op;
 		}
-	}
-
-	// try a new match on input 
-	bool try_match(istream& input, Match& match, streampos& match_end_pos)
-	{
-		bool operator_success = true;
-		stringstream extracted_tmp;
-		string context_end;
-
-		// cout << "\nMatch start" << endl;
-
-		// For each operators
-		auto it = begin();
-		while (operator_success && it != end()) {
-			// cout << "Next char: '" << i_to_string(input.peek()) << "'" << endl;
-			// cout << "Current op: " << ((*it) == nullptr ? "null" : (*it)->toString()) << endl;
-			operator_success = (*it)->execute(input, extracted_tmp, (++it) == end() ? nullptr : *it);
-		}
-
-		if (operator_success) {
-			// cout << "Match operator_success" << endl;
-
-			// Save the pos before the getline
-			match_end_pos = input.tellg();
-
-			getline(input, context_end);
-			match.complete(extracted_tmp, context_end);
-		}
-
-		// cout << "Match failure" << endl;
-		return operator_success;
 	}
 
 	// skip to position but still update line & col on the way
@@ -356,36 +215,6 @@ public:
 	// returns all matches found
 	list<Match> search_in(istream& input)
 	{
-		list<Match> matches;
-
-		// Position
-		string current_line;
-		int line = 1;
-		int col = 1;
-
-		while (input.peek() != EOF) {
-			// Match vars
-			streampos saved_pos = input.tellg(), new_pos;
-			bool match_success = true;
-
-			Match match(line, current_line);
-			match_success = try_match(input, match, new_pos);
-
-			if (match_success) {
-				matches.push_back(match);
-				// Keep new pos
-			}
-			else {
-				// Indent old pos
-				new_pos = saved_pos.operator+(1);
-			}
-
-			// Rewinding to update lines & cols
-			input.seekg(saved_pos);
-			skip_to_pos(input, new_pos, current_line, line, col);
-		}
-
-		return matches;
 	}
 
 	string toString()
@@ -402,16 +231,16 @@ class RegexFactory
 {
 public:
 	// regex parser
-	// create one operator from input & add it to regex
-	static RegexOperator* create_next_op(istream& input, Regex& regex, int& cols)
+	// create one operator from regex & add it to regex
+	static RegexOperator* create_next_op(string& regex, Regex& last, int& cols)
 	{
 		// TODO: implement OR
-		int c = input.get();
+		int c = regex.pop_front();
 		cols++;
 		switch (c) {
 		case '\\':
 		{
-			int antislash_command = input.get();
+			int antislash_command = regex.pop_front();
 			cols++;
 			switch (antislash_command) {
 			case 't':
@@ -437,7 +266,7 @@ public:
 			return new RegexKleenStar();
 		case '|':
 		{
-			if (regex.empty()) {
+			if (last.empty()) {
 				throw RegexError("no option found before | operator", -1, cols);
 			}
 
@@ -448,13 +277,13 @@ public:
 
 			RegexOperator* op;
 			do {
-				op = create_next_op(input, regex, cols);
+				op = create_next_op(regex, last, cols);
 
 				if (op != nullptr) {
 					or_op->options.push_back(op);
 				}
 			}
-			while (op != nullptr && input.peek() == '|' && input.get());
+			while (op != nullptr && regex.peek() == '|' && regex.pop_front());
 
 			if (or_op->options.size() == 1) {
 				throw RegexError("no option found after | operator", -1, cols);
@@ -469,10 +298,10 @@ public:
 		}
 	}
 
-	// regex factory
+	// Regex factory
 	static Regex from_cstr(const char* str)
 	{
-		stringstream input(str);
+		string input(str);
 		Regex regex;
 
 		int cols = 0;
